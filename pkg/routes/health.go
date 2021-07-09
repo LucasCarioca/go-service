9c6331776f40427055826552c42530ed2afa8a5b
Lucas Desouza <lucas.desouza@aa.com>
2021-07-09 01:41:12 -0500

    adding in other services

--- pkg/routes/health.go
+++ pkg/routes/health.go
@@ -10,9 +10,18 @@ func get(context *gin.Context) {
 	message := "up"
 	hs := services.HealthService{}
 
-	pinetHub := hs.CheckStatus("http://localhost:8000/api/v1/health")
-	pinetNode := hs.CheckStatus("http://localhost:8000/api/v1/health")
-	list := [2]services.Status{pinetHub, pinetNode}
+	pinetHub := hs.CheckStatus("192.168.1.150:30101/api/v1/health")
+	pinetNode01 := hs.CheckStatus("http://192.168.1.200:8000/api/v1/health")
+	pinetNode02 := hs.CheckStatus("http://192.168.1.201:8000/api/v1/health")
+	pinetNode03 := hs.CheckStatus("http://192.168.1.203:8000/api/v1/health")
+	pinetNode04 := hs.CheckStatus("http://192.168.1.202:8000/api/v1/health")
+	list := [5]services.Status{
+		pinetHub, 
+		pinetNode01,
+		pinetNode02,
+		pinetNode03,
+		pinetNode04,
+	}
 
 	for _, s := range  list{
 		if s.Message != "up" {
@@ -24,7 +33,10 @@ func get(context *gin.Context) {
 		"message": message,
 		"services": gin.H{
 			"pinet-hub": pinetHub.Message,
-			"pinet-node-01": pinetNode.Message,
+			"pinet-node-01": pinetNode01.Message,
+			"pinet-node-02": pinetNode02.Message,
+			"pinet-node-03": pinetNode03.Message,
+			"pinet-node-04": pinetNode04.Message,
 		},
 	})
 }
